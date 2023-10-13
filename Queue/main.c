bd1cb44ec3a4405b55b73643e8181ef0c90b3889
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-10-13 14:33:45 +0300

    add structures

--- Queue/main.c
+++ Queue/main.c
@@ -1,28 +1,60 @@
 #include <stdio.h>
+#include "queue.h"
 
-typedef struct
+QueueElement* newQueue()
 {
-	int value;
-	QueueElement* next;
-} QueueElement;
-
-typedef struct
-{
-	int front;
-	int back;
-} Queue;
-
-int enqueue(Queue* **head, int newValue)
-{
-
+	QueueElement* head = NULL;
+	head = (QueueElement*)malloc(sizeof(QueueElement));
+	return head;
 }
 
-int isEmpty(Queue* queue)
+int enqueue(QueueElement**head, const int newValue)
+{
+	if (*head == NULL)
+	{
+		return 404;
+	}
+	QueueElement* next = malloc(sizeof(QueueElement));
+	if (next == NULL)
+	{
+		return 404;
+	}
+	next->value = newValue;
+	next->next = *head;
+	*head = next;
+}
+
+int dequeue(QueueElement** head)
 {
 	return 0;
 }
 
+int isEmpty(QueueElement* queue)
+{
+	return queue->value;
+}
+
+int front()
+{
+
+}
+
+int back()
+{
+
+}
+
+void printQueue(QueueElement* queue)
+{
+	for (int i = queue->next; i != NULL; queue->next)
+	{
+		printf("%d ", queue->value);
+	}
+}
+
 int main()
 {
-	Queue* queue = NULL;
+	QueueElement* queue = newQueue();
+	//const int errorCode1 = enqueue(&head, 2);
+	//printf("%d\n", head->value);
 }
--- Queue/queue.h
+++ Queue/queue.h
@@ -1,1 +1,14 @@
 #pragma once
+
+typedef struct
+{
+	int value;
+	Queue* atributes;
+	QueueElement* next;
+} QueueElement;
+
+typedef struct
+{
+	int front;
+	int back;
+} Queue;
