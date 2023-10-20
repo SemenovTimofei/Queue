07d693e4c2f624ecb80d591a4ec06a9351198f53
SemenovTimofei <st117342@student.spbu.ru>
2023-10-20 14:57:04 +0300

    Commit finished task

--- Queue/main.c
+++ Queue/main.c
@@ -1,28 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct
-{
-	int value;
-	QueueElement* next;
-} QueueElement;
-
-typedef struct
-{
-	int front;
-	int back;
-} Queue;
-
-int enqueue(Queue* **head, int newValue)
-{
-
-}
-
-int isEmpty(Queue* queue)
-{
-	return 0;
-}
+#include "queue.h"
 
 int main()
 {
-	Queue* queue = NULL;
+    system("chcp 1251 > nul");
+
+    ErrorCode result = ok;
+
+    Queue* queue;
+
+    printf("Создаем очередь\n");
+    createQueue(&queue);
+
+    if (isEmpty(queue))
+    {
+        printf("Очередь пуста\n\n");
+    }
+    else
+    {
+        printf("Очередь не пуста\n\n");
+    }
+
+    printf("Добавляем элементы в очередь\n");
+    enqueue(queue, 1);
+    enqueue(queue, 2);
+    enqueue(queue, 3);
+
+    printQueue(queue);
+
+    if (isEmpty(queue))
+    {
+        printf("\nОчередь пуста\n\n");
+    }
+    else
+    {
+        printf("\nОчередь не пуста\n\n");
+    }
+
+    printf("%d это первый элемент\n", front(queue, &result));
+    printf("%d это последний элемент\n\n", back(queue, &result));
+
+    printf("Удаляем элемент очереди\n");
+    dequeue(queue);
+
+    printQueue(queue);
+
+    printf("\nУдаляем очередь\n\n");
+    deleteQueue(queue);
+
+    if (isEmpty(queue))
+    {
+        printf("Очередь пуста");
+    }
+    else
+    {
+        printf("Очередь не пуста");
+    }
+
+    free(queue);
+    return 0;
 }
--- Queue/queue.h
+++ Queue/queue.h
@@ -1,1 +1,36 @@
 #pragma once
+
+typedef enum ErrorCode
+{
+	ok,
+	queueIsEmpty,
+	outOfMemory,
+} ErrorCode;
+
+typedef struct Queue Queue;
+
+typedef struct QueueElement QueueElement;
+
+// функция для создания очереди
+ErrorCode createQueue(Queue** queue);
+
+// функция для добавления элемента в очередь
+ErrorCode enqueue(Queue* queue, const int value);
+
+// функция для удаления элемента из очереди
+ErrorCode dequeue(Queue* queue);
+
+// функция для вывода всех элементов очереди
+void printQueue(Queue* queue);
+
+// функция для проверки пустой очереди
+bool isEmpty(Queue* queue);
+
+// функция, возвращающая первый элемент очереди
+int front(Queue* queue, ErrorCode* errorCode);
+
+// функция, возвращающая последний элемент очереди
+int back(Queue* queue, ErrorCode* errorCode);
+
+// функция, удаляющая очередь
+void deleteQueue(Queue* queue);
